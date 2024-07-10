f9d42b210b5bbc27a8700b557e579611a26ac457
Gupta <c2gupt@gmail.com>
2024-07-10 17:42:30 +0530

    first commit

---
+++ Frontend/my-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Footer from "./components/Footer";
+import About from "./components/About";
+export default function App() {
+  return (
+    <div>
+      <About/>
+      <Footer/>
+    </div>
+  )
+}
+
+
+
+
+  
+  
+
+
+
